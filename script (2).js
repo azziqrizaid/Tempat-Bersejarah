d5bdba3b3f18404a83e3a37286fb8d406c71e4f3
azziqrizaid <azziqrizaid@gmail.com>
2025-03-04 20:48:04 +0800

    Tempat Bersejarah
    mempunyai foto-foto tempat bersejarah

---
+++ script (2).js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    new Swiper(".swiper-container", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
+
+
+
+
